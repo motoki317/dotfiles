5ef1ce705f36cfb9cffe60a79d5709eac985aaa1
motoki317 <motoki317@gmail.com>
2025-10-21 13:54:31 +0900

    feat: focus main buffer

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -5,6 +5,9 @@
 -- Neo-tree: focus or open if closed
 vim.keymap.set("n", "<leader>e", "<cmd>Neotree focus<cr>", { desc = "Focus Neo-tree" })
 
+-- Focus main buffer pane (move to right window)
+vim.keymap.set("n", "<leader>.", "<cmd>wincmd l<cr>", { desc = "Focus main buffer" })
+
 -- Close buffer with Ctrl+w (same as <leader>bd)
 vim.keymap.set("n", "<C-w>", function()
   Snacks.bufdelete()
